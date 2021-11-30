0f9bbf2ba835b54477a8f409561f8fa864eef2e2
json96ua <Heavymetal1>
2021-12-01 01:34:29 +0200

    Modify Ticket class, make all variables as instance variables

--- model/ticket.py
+++ model/ticket.py
@@ -2,12 +2,12 @@ import uuid
 
 
 class Ticket:
-    __code = ''
-    __ticker_owner_nickname = ''
 
     def __init__(self, user):
         self.__code = user.nickname + str(uuid.uuid4())
         self.__ticker_owner_nickname = user.nickname
+        self.__code = ''
+        self.__ticker_owner_nickname = ''
 
     def get_ticket_code(self):
         return self.__code
