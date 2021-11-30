3a055e5e52c1519ee7c0025cb19703bccda33ba2
json96ua <Heavymetal1>
2021-12-01 01:35:08 +0200

    Add a method for getting info about a pool from the ThorChain

--- thorchain/api.py
+++ thorchain/api.py
@@ -2,6 +2,7 @@ import requests
 
 api_url = 'https://testnet.midgard.thorchain.info/v2/'
 member_url = 'member/'
+pool_url = 'pool/'
 
 
 def is_pool_participant(user):
@@ -12,3 +13,11 @@ def get_member_details(user):
     response = requests.get(api_url + member_url + str(user.user_id))
     if response.status_code != 404:
         return response.json()
+
+
+def get_pool_info_by_name(pool_name):
+    try:
+        response = requests.get(f'https://testnet.midgard.thorchain.info/v2/{pool_url}/{pool_name}')
+        return response.json()
+    except ValueError:
+        raise
